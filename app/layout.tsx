64190a33731da0519c3f32632197ce3d6b2132b3
devin9754 <devin9754@gmail.com>
2025-03-08 09:19:51 -0800

    dfh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-// Import the client-side AmplifyProvider
-import AmplifyProvider from "./AmplifyProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +19,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AmplifyProvider>{children}</AmplifyProvider>
+      <body
+        className={${geistSans.variable} ${geistMono.variable} antialiased}
+      >
+        {children}
       </body>
     </html>
   );
